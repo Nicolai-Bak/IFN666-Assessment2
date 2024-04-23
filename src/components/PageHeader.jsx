d643c06b1b527e3314b8a97b8e3986dfacb3b554
Nicolai-Bak <nicolai.bak@gmail.com>
2024-04-23 21:17:28 +1000

    updating about page

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -4,7 +4,6 @@ export function PageHeader({ children }) {
   return (
     <Typography
       variant='h6'
-      component='div'
       sx={{
         fontWeight: 'bold',
         color: 'primary.main',
