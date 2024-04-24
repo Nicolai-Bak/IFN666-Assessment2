e1e47049bd630f0e1f51c565bae94398b046f6ba
Nicolai-Bak <nicolai.bak@gmail.com>
2024-04-24 19:45:25 +1000

    updating portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, Skeleton } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 import { strava_access_token } from '../../secrets';
@@ -6,14 +7,31 @@ export function Portfolio() {
   const { isLoading, activities, error } = useStravaActivities();
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return Array.from({ length: 5 }).map((_, index) => (
+      <Skeleton key={index} variant='rectangular' sx={{ height: '128px', marginBottom: '16px' }} />
+    ));
   }
 
   if (error) {
-    return <p>{error.message}</p>;
+    return (
+      <>
+        <Card sx={{ backgroundColor: 'error.main' }}>
+          <CardHeader title={error.message} sx={{ textAlign: 'center', color: 'text.secondary' }} />
+        </Card>
+      </>
+    );
   }
 
-  return <p>{JSON.stringify(activities)}</p>;
+  return (
+    <>
+      {activities.map((activity) => (
+        <Card raised key={activity.id} sx={{ backgroundColor: 'transparent', marginBottom: '16px' }}>
+          <CardHeader title={activity.name} />
+          <CardContent>{activity.start_date}</CardContent>
+        </Card>
+      ))}
+    </>
+  );
 }
 
 const strava_base_url = 'https://www.strava.com/api/v3';
@@ -25,7 +43,12 @@ function useStravaActivities() {
 
   useEffect(() => {
     fetch(`${strava_base_url}/athlete/activities?access_token=${strava_access_token}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 401) {
+          throw new Error('Unauthorized');
+        }
+        return response.json();
+      })
       .then((data) => setActivities(data))
       .then(() => setIsLoading(false))
       .catch((error) => setError(error));
