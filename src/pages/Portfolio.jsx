62e50ba7b0b2867f361ab1d17fa0853b5709d6cd
Nicolai-Bak <nicolai.bak@gmail.com>
2024-04-27 18:36:01 +1000

    adding filter on activity type

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardHeader, Skeleton } from '@mui/material';
+import { Card, CardContent, CardHeader, FormControl, InputLabel, MenuItem, OutlinedInput, Select, Skeleton } from '@mui/material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useStravaActivities } from '../hooks/useStravaActivities';
@@ -6,20 +7,52 @@ import { ErrorCard } from './ErrorCard';
 
 export function Portfolio() {
   const { isLoading, activities, error } = useStravaActivities();
+  const [activityTypes, setActivityTypes] = useState([]);
 
   if (isLoading) {
-    return Array.from({ length: 5 }).map((_, index) => (
-      <Skeleton key={index} variant='rectangular' sx={{ height: '128px', marginBottom: '16px' }} />
-    ));
+    return (
+      <>
+        <Skeleton variant='rectangular' sx={{ width: '300px', height: '56px', marginBottom: '16px' }} />
+        {Array.from({ length: 5 }).map((_, index) => (
+          <Skeleton key={index} variant='rectangular' sx={{ height: '128px', marginBottom: '16px' }} />
+        ))}
+      </>
+    );
   }
 
   if (error) {
     return <ErrorCard errorMessage={error.message} />;
   }
 
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setActivityTypes(typeof value === 'string' ? value.split(',') : value);
+  };
+
   return (
     <>
-      {activities.map((activity) => (
+      <FormControl sx={{ marginBottom: '16px', width: 300 }}>
+        <InputLabel id='type-selector'>Activity types</InputLabel>
+        <Select
+          labelId='type-selector'
+          multiple
+          value={activityTypes}
+          onChange={handleChange}
+          input={<OutlinedInput label='Activity types' />}
+        >
+          {activities
+            .map((activity) => activity.type)
+            .filter((value, index, self) => self.indexOf(value) === index)
+            .map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+      {(activityTypes.length > 0 ? activities.filter((activity) => activityTypes.includes(activity.type)) : activities).map((activity) => (
         <Link key={activity.id} to={`${activity.id}`} style={{ textDecoration: 'none' }}>
           <Card raised sx={{ backgroundColor: 'transparent', marginBottom: '16px' }}>
             <CardHeader title={activity.name} />
