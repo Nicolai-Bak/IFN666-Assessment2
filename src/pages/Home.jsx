fae199d1d53b06160602f249c865bd738c5a0f81
Nicolai-Bak <nicolai.bak@gmail.com>
2024-04-28 13:17:49 +1000

    updating home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
+import { Typography } from '@mui/material';
+
 export function Home() {
-  return <p>Home</p>;
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+        <div style={{ minWidth: '600px', width: '50%' }}>
+          <img
+            style={{ minWidth: '100%', width: '50%', borderRadius: '4px', borderColor: 'transparent' }}
+            src='/public/20240416_100457180_iOS.jpg'
+            alt='Surf hero image'
+          />
+          <Typography variant='h5' sx={{ mt: '2rem' }}>
+            Welcome to my page! Have a look around to learn more about me, my professional experience, and my latest workouts.
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
 }
