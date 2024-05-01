589ad8deade8b3b9ea7a0c5c955ae4a6b8e6af09
Nicolai-Bak <nicolai.bak@gmail.com>
2024-05-01 19:23:09 +1000

    correcting activitycard

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -7,15 +7,18 @@ export function ActivityCard({ activity }) {
   return (
     <CardBase title={activity.name} img={<ActivityIcon type={activity.type} />}>
       <Typography variant='body2'>
-        Distance: {activity.distance} meters
+        {/* Add comma as a thousands separator */}
+        Distance: {activity.distance.toLocaleString()} m
         <br />
-        Moving Time: {activity.moving_time} seconds
+        {/* converts moving time from seconds to hours:minutes:seconds */}
+        Moving Time: {new Date(activity.moving_time * 1000).toISOString().slice(11, 11 + 8)}
         <br />
         Sport Type: {activity.type}
         <br />
-        Start Date: {new Date(activity.start_date).toLocaleDateString()}
+        Start Date: {activity.start_date.split('T')[0]}
         <br />
-        Average Speed: {activity.average_speed} m/s
+        {/* converts average speed from meters/second to km/hour */}
+        Average Speed: {(activity.average_speed * 3.6).toFixed(2)} km/h
         <br />
         Average Heart Rate: {activity.average_heartrate} bpm
       </Typography>
